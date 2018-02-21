61773a66151560c74e6454eb051f39b76fcbf6f0
Ryan Shaw <ryan@rshaw.me>
2018-02-21 08:29:20 +0000

    Update build file

--- src/build.sh
+++ src/build.sh
@@ -7,8 +7,12 @@ mkdir -p /output/linux_x86/
 
 echo "Configuring build"
 pushd /garlicoin
+git checkout master
+git pull
+git describe
+
 ./autogen.sh
-./configure --with-gui=no --disable-tests --disable-gui-tests --disable-shared --enable-cxx
+./configure --with-gui=no --disable-tests --disable-gui-tests --disable-shared --enable-cxx --disable-bench
 
 echo "Starting compile"
 make clean
@@ -17,5 +21,11 @@ make -j2
 cp /garlicoin/src/garlicoind /output/linux_x86/
 cp /garlicoin/src/garlicoin-cli /output/linux_x86/
 cp /garlicoin/src/garlicoin-tx /output/linux_x86/
+
+pushd /output/linux_x86/
+
+./garlicoind --version
+
+tar -cvzf garlicoin-0.16.0.2-x86_64-linux-gnu.tar.gz garlicoind garlicoin-cli
 
 echo "Done!"
